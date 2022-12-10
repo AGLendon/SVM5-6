7ff44d131341fefd5fef315d8850ee6bb2b7d4df
korvgubben123 <120194403+korvgubben123@users.noreply.github.com>
2022-12-10 16:22:58 +0100

    frequency

--- LaserScript.m
+++ LaserScript.m
@@ -9,7 +9,7 @@ clear bg; clear pos12; clear pos34; clear pos56; clear pos78; clear u_pos34;
 % Data loaded in a struct access like: Fs = laser.Fs
 
 %%
-f=(1:1:6400);
+f=(1:1:64000);
 
 
 Avrg_coherence = mean(Coherence);
